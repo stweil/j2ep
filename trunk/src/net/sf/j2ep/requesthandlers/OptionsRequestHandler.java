f19f90e2b83c28c62d9d3fa058bab637620788a2
anders_n <anders_n@fcd65fa4-a2b5-4591-ae2a-c1fec51234fc>
2005-07-22 10:58:57 +0000

    Added javadoc

--- trunk/src/net/sf/j2ep/requesthandlers/OptionsRequestHandler.java
+++ trunk/src/net/sf/j2ep/requesthandlers/OptionsRequestHandler.java
@@ -9,8 +9,20 @@ import org.apache.commons.httpclient.methods.OptionsMethod;
 
 import net.sf.j2ep.RequestHandlerBase;
 
+/**
+ * Handler for the OPTIONS method.
+ *
+ * @author Anders Nyman
+ */
 public class OptionsRequestHandler extends RequestHandlerBase {
 
+    /**
+     * @see net.sf.j2ep.RequestHandler#process(javax.servlet.http.HttpServletRequest, java.lang.String)
+     * 
+     * Sets the headers and does some checking for if this request
+     * is meant for the server or for the proxy. This check is done
+     * by looking at the Max-Forwards header. 
+     */
     public HttpMethod process(HttpServletRequest request, String url) throws IOException {
         OptionsMethod method = new OptionsMethod(url);
         setHeaders(method, request);
@@ -26,8 +38,7 @@ public class OptionsRequestHandler extends RequestHandlerBase {
             }
 
         } catch (NumberFormatException e) {
-            throw new IOException(
-                    "Incomming request had a Max-Forwards, but didn't contain integer.");
+           
         }
         
         return method;
