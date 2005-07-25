a854a1f08b510d3bed74116ca7c70c014dba7733
anders_n <anders_n@fcd65fa4-a2b5-4591-ae2a-c1fec51234fc>
2005-07-25 16:06:33 +0000

    Have added support for link rewriting

--- trunk/src/net/sf/j2ep/UrlRewritingOutputStream.java
+++ trunk/src/net/sf/j2ep/UrlRewritingOutputStream.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.sf.j2ep;
 
 import java.io.ByteArrayOutputStream;
@@ -9,44 +25,81 @@ import javax.servlet.ServletOutputStream;
 
 public class UrlRewritingOutputStream extends ServletOutputStream {
     
+    /** 
+     * The stream we are wrapping, is the original response stream.
+     */
     private ServletOutputStream originalStream;
+    
+    /** 
+     * Stream that is written to, works as a buffer for the response stream.
+     */
     private ByteArrayOutputStream stream;
     
-    
+    /** 
+     * Regex matching links in the HTML.
+     */
+    private static Pattern linkPattern = Pattern.compile("\\b(href=|src=|action=)([\"?\'?])(/[^\"\'\\s>]+)[\"?\'?\\s]", Pattern.CASE_INSENSITIVE | Pattern.CANON_EQ);
+
+    /**
+     * Basic constructor.
+     * 
+     * @param originalStream The stream we are wrapping
+     */
     public UrlRewritingOutputStream(ServletOutputStream originalStream) {
         this.originalStream = originalStream;
         stream = new ByteArrayOutputStream();
     }
 
-
+    /**
+     * @see java.io.OutputStream#write(int)
+     */
     public void write(int b) throws IOException {
         stream.write(b);        
     }
     
+    /**
+     * @see java.io.OutputStream#write(byte[], int, int)
+     */
     public void write(byte[] b, int off, int len) throws IOException {
         stream.write(b, off, len);
     }
     
+    /**
+     * @see java.io.OutputStream#write(byte[])
+     */
     public void write(byte[] b) throws IOException {
         stream.write(b);
     }
     
-    public void rewrite() throws IOException {
-        Pattern linkPattern = Pattern.compile("\\b(href=|src=)\"?([^\"\\s>]+)[\"?\\s]", Pattern.CASE_INSENSITIVE | Pattern.CANON_EQ);
+    /**
+     * Processes the stream looking for links, all links
+     * found are rewritten. After this the stream is written
+     * to the response.
+     * 
+     * @param rule The rule knowing how to rewrite
+     * @throws IOException Is thrown when there is a problem with the streams
+     */
+    public void rewrite(Rule rule) throws IOException {
         
         StringBuffer page = new StringBuffer();
+        
         Matcher matcher = linkPattern.matcher(stream.toString());
         while (matcher.find()) {
-           String link = matcher.group(2);
-           matcher.appendReplacement(page, "$1\"" + link + "\"");
+           String link = rule.revert(matcher.group(3));
+           System.out.println(link);
+           matcher.appendReplacement(page, "$1$2" + link + "$2");
         }
         matcher.appendTail(page);
-        
         originalStream.print(page.toString());
         stream.close();
         originalStream.close();
     }
     
+    /**
+     * Sends the output to the original response stream without
+     * doing any link rewriting.
+     * @throws IOException Is thrown when there is a problem with the streams
+     */
     public void process() throws IOException {
         originalStream.write(stream.toByteArray());
         stream.close();
--- trunk/src/net/sf/j2ep/UrlRewritingResponseWrapper.java
+++ trunk/src/net/sf/j2ep/UrlRewritingResponseWrapper.java
@@ -1,6 +1,8 @@
 package net.sf.j2ep;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
@@ -8,23 +10,119 @@ import javax.servlet.http.HttpServletResponseWrapper;
 
 public class UrlRewritingResponseWrapper extends HttpServletResponseWrapper{
     
+    /** 
+     * Stream we are using for the response.
+     */
     private UrlRewritingOutputStream outStream;
+    
+    /** 
+     * The response we are wrapping
+     */
     private HttpServletResponse response;
+    
+    /** 
+     * Rule used for this page
+     */
+    private Rule rule;
+    
+    /** 
+     * The location for this server, used when we rewrite absolute URIs
+     */
+    private String server;
+    
+    /** 
+     * Regex to find absolute links.
+     */
+    private static Pattern linkPattern = Pattern.compile("\\b([^/]+://)([^/]+)([\\w/]+)", Pattern.CASE_INSENSITIVE | Pattern.CANON_EQ);
 
-    public UrlRewritingResponseWrapper(HttpServletResponse response) throws IOException {
+    /**
+     * Basic constructor.
+     * 
+     * @param response The response we are wrapping
+     * @param rule The rule that was matched
+     * @param server String we are rewriting servers to
+     * @throws IOException When there is a problem with the streams
+     */
+    public UrlRewritingResponseWrapper(HttpServletResponse response, Rule rule, String server) throws IOException {
         super(response);
         this.response = response;
+        this.rule = rule;
+        this.server = server;
         
         outStream = new UrlRewritingOutputStream(response.getOutputStream());
     }
     
+    /**
+     * Checks if we have to rewrite the header and
+     * if so will rewrite it.
+     * 
+     * @see javax.servlet.http.HttpServletResponse#addHeader(java.lang.String, java.lang.String)
+     */
+    public void addHeader(String name, String originalValue) {
+        String value;
+        if (name.toLowerCase().equals("location")) {
+            value = rewriteHeader(originalValue);
+        } else {
+            value = originalValue;
+        }
+        super.addHeader(name, value);
+    }
+    
+    /**
+     * Checks if we have to rewrite the header and
+     * if so will rewrite it.
+     * 
+     * @see javax.servlet.http.HttpServletResponse#setHeader(java.lang.String, java.lang.String)
+     */
+    public void setHeader(String name, String originalValue) {
+        String value;
+        if (name.toLowerCase().equals("location")) {
+            value = rewriteHeader(originalValue);
+        } else {
+            value = originalValue;
+        }
+        super.setHeader(name, value);
+    }
+
+    
+    /**
+     * Rewrites the header. Will first locate any
+     * links in the header and then rewrite them.
+     * 
+     * @param value The header value we are to rewrite
+     * @return A rewritten header
+     */
+    private String rewriteHeader(String value) {
+        StringBuffer header = new StringBuffer();
+
+        Matcher matcher = linkPattern.matcher(value);
+        while (matcher.find()) {
+            String link = rule.revert(matcher.group(3));
+            matcher.appendReplacement(header, "$1" + server + link);
+        }
+        matcher.appendTail(header);
+        
+        return header.toString();
+    }
+    
+    /**
+     * Returns out own output stream that can be rewritten.
+     * 
+     * @see javax.servlet.ServletResponse#getOutputStream()
+     */
     public ServletOutputStream getOutputStream() throws IOException {
         return outStream;
     }
     
-    public void rewrite() throws IOException {
-        if (response.getContentType().contains("text")) {
-            outStream.rewrite();
+    /**
+     * Rewrites the output stream to change any links.
+     * 
+     * @throws IOException Is thrown when there is a problem with the streams
+     */
+    public void rewriteStream() throws IOException {
+        String contentType = response.getContentType();
+        if (contentType != null && rule.isRewriting() && (contentType.contains("text") || contentType.contains("script"))) {
+            outStream.rewrite(rule);
         } else {
             outStream.process();
         }
