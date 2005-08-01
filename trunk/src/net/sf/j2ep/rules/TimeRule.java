a1afeba7cab7b7f7c64b0a00f0430f1105f7f586
anders_n <anders_n@fcd65fa4-a2b5-4591-ae2a-c1fec51234fc>
2005-08-01 16:30:10 +0000

    A simple rule to check the time. Started to do a bigger class with week day but I think that this is rather sufficient.

---
+++ trunk/src/net/sf/j2ep/rules/TimeRule.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2005 Anders Nyman.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sf.j2ep.rules;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * A simple rule that checks the hour. If the hour
+ * is in the specified range we will allow the 
+ * request.
+ *
+ * @author Anders Nyman
+ */
+public class TimeRule extends BaseRule {
+    
+    /** 
+     * The start hour
+     */
+    private int startTime;
+    
+    /** 
+     * The end hour
+     */
+    private int endTime;
+
+    /**
+     * @see net.sf.j2ep.Rule#matches(javax.servlet.http.HttpServletRequest)
+     */
+    public boolean matches(HttpServletRequest request) {
+        int currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        return (currentTime >= startTime && currentTime <= endTime);        
+    }
+    
+    /**
+     * Sets the start hour that requests will be allowed.
+     * 
+     * @param time The hour
+     */
+    public void setStartTime(String time) {
+        if (time == null) {
+            throw new IllegalArgumentException("The start time cannot be null");
+        } else {
+            startTime = Integer.parseInt(time);
+        }
+    }
+    
+    /**
+     * Sets the end hour that request will be allowed.
+     * 
+     * @param time The hour
+     */
+    public void setEndTime(String time) {
+        if (time == null) {
+            throw new IllegalArgumentException("The end time cannot be null");
+        } else {
+            endTime = Integer.parseInt(time);
+        }
+    }
+
+}
