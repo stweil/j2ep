0c4cbe53dee9ddc0249f6740cad148dad365d04c
anders_n <anders_n@fcd65fa4-a2b5-4591-ae2a-c1fec51234fc>
2005-08-19 08:05:35 +0000

    Added javadoc

--- src/net/sf/j2ep/model/AllowedMethodHandler.java
+++ src/net/sf/j2ep/model/AllowedMethodHandler.java
@@ -21,13 +21,23 @@ import java.util.StringTokenizer;
 
 import org.apache.commons.httpclient.HttpMethod;
 
+/**
+ * A utility class that will be used throughout the 
+ * project to process headers to filter out those
+ * not allowed by the proxy.
+ *
+ * @author Anders Nyman
+ */
 public class AllowedMethodHandler {
     
     /** 
-     * The methods handled by this factory.
+     * The methods handled by the proxy.
      */
-    private static String allowString = "OPTIONS,GET,HEAD,POST,PUT,DELETE,TRACE";
+    private static String allowString;
     
+    /** 
+     * A set of the HTTP methods allowed by the proxy.
+     */
     private static HashSet allowedMethods;
     
     /**
@@ -53,7 +63,7 @@ public class AllowedMethodHandler {
     }
     
     /**
-     * Returns the allow methods for this factory.
+     * Returns the allow methods for this proxy.
      * 
      * @return Allowed methods
      */ 
@@ -61,14 +71,33 @@ public class AllowedMethodHandler {
         return allowString;
     }
     
+    /**
+     * Will check if the specified method is allowed by
+     * looking if it is included in the allowedMethods.
+     * 
+     * @param method The method that is checked
+     * @return true if the method is allowed, false otherwise
+     */
     public static boolean methodAllowed(String method) {
         return allowedMethods.contains(method.toUpperCase());
     }
     
+    /**
+     * Checks the method to see if it's allowed
+     * 
+     * @param method The method that is checked
+     * @return true if the method is allowed, false otherwise
+     * @see AllowedMethodHandler#methodAllowed(String)
+     */
     public static boolean methodAllowed(HttpMethod method) {
         return methodAllowed(method.getName());
     }
     
+    /**
+     * Will set the allowed methods, both by setting the string
+     * and also by adding all the methods to the set of allowed.
+     * @param allowed The list of allowed methods, should be comma separated
+     */
     public synchronized static void setAllowedMethods(String allowed) {
         allowedMethods = new HashSet();
         allowString = allowed;
